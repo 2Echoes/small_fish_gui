18b164a2472d9976bdaa7d7d2e65d0424dbc8288
Floric Slimani <floric.slimani@live.fr>
2024-03-28 16:09:09 +0100

    init start

---
+++ start.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    try:
+        import small_fish.pipeline.main
+    except ValueError as ve:
+        return str(ve)
+
+if __name__ == "__main__":
+    sys.exit(main())
