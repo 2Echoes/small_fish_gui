239673bf44dde7ab465be27f864d8a38faf96bc9
Floric Slimani <floric.slimani@live.fr>
2024-06-11 12:27:06 +0200

    Error popup if user encounters unforseen bug

--- __main__.py
+++ __main__.py
@@ -1,9 +1,15 @@
 import sys
 import os
+import PySimpleGUI as sg
 
 def main():
     import small_fish.pipeline.main
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    try :
+        sys.exit(main())
+    except Exception as error :
+        sg.popup("Sorry. Something went wrong...\nIf you have some time to spare could you please communicate the error you encountered (next window) on :\nhttps://github.com/2Echoes/small_fish/issues.")
+        sg.popup_error_with_traceback(error)
+        raise error
