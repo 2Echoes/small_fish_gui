66a5bbc52ee885c1297b6e52cc5e06a0cf45280e
Floric Slimani <floric.slimani@live.fr>
2024-06-25 17:42:29 +0200

    add output to csv

--- interface/output.py
+++ interface/output.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from bigfish.stack import check_parameter
 
-
+MAX_LEN_EXCEL = 1048576
 
 def _cast_spot_to_tuple(spot) :
     return tuple([coord for coord in spot])
@@ -10,11 +10,11 @@ def _cast_spot_to_tuple(spot) :
 def _cast_spots_to_tuple(spots) :
     return tuple(list(map(_cast_spot_to_tuple, spots)))
 
-def write_results(dataframe: pd.DataFrame, path:str, filename:str, do_excel= True, do_feather= False) :
+def write_results(dataframe: pd.DataFrame, path:str, filename:str, do_excel= True, do_feather= False, do_csv=False) :
     check_parameter(dataframe= pd.DataFrame, path= str, filename = str, do_excel = bool, do_feather = bool)
 
     if len(dataframe) == 0 : return True
-    if not do_excel and not do_feather : 
+    if not do_excel and not do_feather and not do_csv : 
         return False
 
     if not path.endswith('/') : path +='/'
@@ -23,7 +23,7 @@ def write_results(dataframe: pd.DataFrame, path:str, filename:str, do_excel= Tru
 
     new_filename = filename
     i= 1
-    while new_filename + '.xlsx' in os.listdir(path) or new_filename + '.feather' in os.listdir(path) :
+    while new_filename + '.xlsx' in os.listdir(path) or new_filename + '.feather' in os.listdir(path) or new_filename + '.csv' in os.listdir(path) :
         new_filename = filename + '_{0}'.format(i)
         i+=1
 
@@ -36,7 +36,14 @@ def write_results(dataframe: pd.DataFrame, path:str, filename:str, do_excel= Tru
     if 'clusters' in dataframe.columns : 
         dataframe = dataframe.drop(['clusters'], axis= 1)
 
-    if do_excel : dataframe.reset_index(drop=True).to_excel(path + filename + '.xlsx')
-    if do_feather : dataframe.reset_index(drop=True).to_feather(path + filename + '.feather')
+    if do_feather : dataframe.reset_index(drop=True).to_feather(path + new_filename + '.feather')
+    if do_csv : dataframe.reset_index(drop=True).to_csv(path + new_filename + '.csv', sep=";")
+    if do_excel : 
+        if len(dataframe) < MAX_LEN_EXCEL :
+            dataframe.reset_index(drop=True).to_excel(path + new_filename + '.xlsx')
+        else : 
+            print("Error : Table too big to be saved in excel format.")
+            return False
+    
 
     return True
